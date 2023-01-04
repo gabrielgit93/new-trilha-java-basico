c7f772814fba929c99e4920fa19c629e5ddaf0c4
gabrielgit93 <gsgabriel431@gmail.com>
2023-01-04 11:36:40 -0300

    first commit

--- tipos-variaveis/src/Main.java
+++ tipos-variaveis/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto=1;
+        int numeroNorma = numeroCurto;
+        short numeroNormal = numeroCurto;
         System.out.println("Hello world!");
+
+        int numero = 1;
+
+        numero = 10;
+
+        System.out.println("numero");
     }
 }
