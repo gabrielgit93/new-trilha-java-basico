a638192e8e5ccbdfd05d8cdf4e828d4c49d45cb9
gabrielgit93 <gsgabriel431@gmail.com>
2023-01-04 11:51:18 -0300

    first commit

--- tipos-variaveis/src/Main.java
+++ tipos-variaveis/src/Main.java
@@ -1,17 +1,25 @@
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        // tipos primitivos
+        //estudem a classe String que representa texto na aplicação
+
+        String meuNome = "GABRIEL GOULART";
+
         double salarioMinimo = 2500;
 
         short numeroCurto=1;
-        int numeroNorma = numeroCurto;
-        short numeroNormal = numeroCurto;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
         System.out.println("Hello world!");
 
-        int numero = 1;
+        int numero = 5;
 
         numero = 10;
 
         System.out.println("numero");
+
+        double VALOR_DE_PI = 3.14;
+        VALOR_DE_PI = 10.75;
     }
 }
